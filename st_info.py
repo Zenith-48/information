31b381b55e196af1f290301ff443e44f791fa629
Zenith-48 <shalinamaniyar@gmail.com>
2025-11-04 11:54:38 +0530

    Refactor input handling and print statements

--- st_info.py
+++ st_info.py
@@ -1,14 +1,16 @@
 import sys
-if len(sys.argv)==3:
-script name=sys.argv[0]
-name=sys.argv[1]
-rollno=sys.argv[2]
-print("User provided input values:")
+
+if len(sys.argv) == 3:
+    script_name = sys.argv[0]
+    name = sys.argv[1]
+    rollno = sys.argv[2]
+    print("User provided input values:")
 else:
-script name=sys.argv[0]
-name="Shalina"
-rollno="61"
-print("No input given-using default values:")
-print("Script Name:",script name)
-print("Student Name:",name)
-print("Roll No:",rollno)
+    script_name = sys.argv[0]
+    name = "Shalina"
+    rollno = "61"
+    print("No input given - using default values:")
+
+print("Script Name:", script_name)
+print("Student Name:", name)
+print("Roll No:", rollno)
